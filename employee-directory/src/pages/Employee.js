f63612a90de17ea29481bf28fa3d6b41522b047e
Paige Burlew <paigeburlew@gmail.com>
2020-12-22 14:24:52 -0600

    testing

--- employee-directory/src/pages/Employee.js
+++ employee-directory/src/pages/Employee.js
@@ -2,16 +2,34 @@ import React from "react";
 import API from "../utils/API";
 import SearchInput from "../components/SearchInput";
 
-class Component {
+class Form extends Component {
+    state = {
+        firstName: "",
+        lastName: "",
+        
+    }
 
-};
-
-componentDidMount(){
+//componentDidMount(){
+   // API.viewAll()
+    //    .then()
 
 }
 
-handleInputChange () {
+handleInputChange = event => {
+     // Getting the value and name of the input which triggered the change
+     let value = event.target.value;
+    const name = event.target.name;
+
+    if (name === "password") {
+      value = value.substring(0, 15);
+    }
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
 
 };
+
 
 ///the main file for everything to render 
