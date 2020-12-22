35a00b628b15c1a03e613b7c3882edf6942d7e94
Paige Burlew <paigeburlew@gmail.com>
2020-12-22 14:16:30 -0600

    set up handle input change

--- employee-directory/src/components/SearchInput/index.js
+++ employee-directory/src/components/SearchInput/index.js
@@ -1,4 +1,19 @@
 import React from "react";
 
 
+
+function SearchInput(props) {
+    return (
+        <div className="search">
+            <input 
+            placeholder="Search for Employee"
+            type="search"
+            name="employee"
+            type="text"
+            onChange={props.handleInputChange} 
+            />
+        </div>
+    )
+}
+
 export default SearchInput;
