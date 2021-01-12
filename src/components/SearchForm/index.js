8e4fa9d7d6687c10f947d364656948d999170331
Paige Burlew <paigeburlew@gmail.com>
2021-01-11 19:21:18 -0600

    searchform

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -4,16 +4,34 @@ import React from "react";
 
 function SearchForm(props) {
     return (
-        <div className="search">
-            <input 
-            placeholder="Search for Employee"
-            type="search"
-            name="employee"
-            type="text"
-            onChange={props.handleInputChange} 
-            />
-        </div>
-    )
+        <form className="search">
+			<div className="form-group">
+				<input
+					value={props.search}
+					onChange={props.handleInputChange}
+					name="employee"
+					list="employees"
+					type="text"
+					className="form-whole"
+					placeholder="Start typing a name to begin"
+					id="employee"
+				/>
+				<datalist id="employees">
+					{props.employees.map(employee => (
+						<option value={employee.name} key={props.employees.name} />
+					))}
+				</datalist>
+				<button
+					type="submit"
+					onClick={props.handleFormSubmit}
+					className="btn btn-warning"
+				>
+					Search
+				</button>
+			</div>
+		</form>
+	);
 }
+
 
 export default SearchForm;
