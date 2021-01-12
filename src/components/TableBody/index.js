af592931f254f485ac89e9c1b57fc87361982af0
Paige Burlew <paigeburlew@gmail.com>
2021-01-11 20:33:11 -0600

    tb set up

--- src/components/TableBody/index.js
+++ src/components/TableBody/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
-//import "./style.css";
+import "./style.css";
 
 
+function TableBody(){
 
 
+}
+
+return (
+    <div className="table-body">
+        {props.children}
+    </div>
+)
 export default TableBody;
